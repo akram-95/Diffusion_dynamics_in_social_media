4e58034b02c17c3ec91b8c8b813b1bf5a34d161f
Akram <akram.chorfi@stud.uni-hannover.de>
2024-04-15 17:20:13 +0200

    implement building network analysis

--- manage.py
+++ manage.py
@@ -2,6 +2,78 @@
 """Django's command-line utility for administrative tasks."""
 import os
 import sys
+import json
+import gzip
+
+from tqdm import tqdm
+from elasticsearch.helpers import scan
+from elasticsearch import Elasticsearch
+
+from api import utils
+from api.services import ApiService
+
+# from api.services import ApiService
+# from api.utils import parse_json
+
+es = Elasticsearch(
+    "http://hadoop3.kbs.uni-hannover.de:9300",
+    basic_auth=("akram.chorfi", "yi6riaNeijiyiephoo5r"),
+    request_timeout=30,
+    retry_on_timeout=True,
+)
+
+INDEX_TWEETS = "twitter-politics-tweets"
+INDEX_REPLIES = "twitter-politics-tweets_reply"
+INDEX_QUOTE = "twitter-politics-tweets_quote"
+fields_tweet = [
+    "id",
+    "user_id",
+    "date",
+    "text",
+    "lang",
+    "translation2",
+    "sentiment"
+]
+fields_reply = [
+    "id",
+    "user_id",
+    "in_reply_to_tweet_id",
+    "date",
+    "text",
+    "lang",
+    "translation2",
+    "sentiment"
+]
+fields_quote = [
+    "id",
+    "user_id",
+    "in_reply_to_tweet_id",
+    "date",
+    "text",
+    "lang",
+    "translation2",
+    "sentiment"
+]
+
+q = {
+    "query": {
+        "bool": {
+            "filter": [
+                {"term": {
+                    "lang": {
+                        "value": "de",
+                    }
+                }},
+                {"range": {
+                    "date": {
+                        "gte": "2021-01-01",
+                        "lte": "2024-01-01"
+                    }
+                }}
+            ]
+        }
+    }
+}
 
 
 def main():
@@ -19,4 +91,18 @@ def main():
 
 
 if __name__ == '__main__':
+    """
+
+    with gzip.open(f"{INDEX_REPLIES}.jsonl.gz", "wt") as f:
+        for hit in tqdm(scan(
+                es, index=INDEX_REPLIES, size=10000, query={**q, "_source": fields_reply, "sort": ["_doc"]}
+        )):
+            f.write(json.dumps(hit["_source"]) + "\n")
+            
+            
+    """
+
+    # print(utils.calculate_cosine_sim("I love horror movies", "Lights out is a horror movie"))
+    ApiService().extract_topics_with_represented_docs()
+
     main()
